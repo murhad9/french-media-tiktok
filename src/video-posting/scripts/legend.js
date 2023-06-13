d8f7b12da518b0b7fe1c8f8b2a4cd9b417000040
Aymen Chebbi <chebbi.aymen@hotmail.com>
2023-06-12 22:12:35 -0400

    Add heatmap legend

--- src/video-posting/scripts/legend.js
+++ src/video-posting/scripts/legend.js
@@ -57,5 +57,33 @@ export function initLegendAxis () {
  * @param {*} colorScale The color scale represented by the legend
  */
 export function draw (x, y, height, width, fill, colorScale) {
-  // TODO : Draw the legend
+  console.log('drawing legend')
+  d3.select('.legend.bar')
+    .attr('x', x)
+    .attr('y', y)
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', fill)
+
+  const ticks = colorScale.ticks()
+  d3.select('.legend.axis')
+    .selectAll('text')
+    .data(ticks)
+    .enter()
+    .append('text')
+    .style('font', '10px sans-serif')
+    .style('fill', '#777')
+    .attr('x', function (d, i) {
+      return x - 10
+    })
+    .attr('text-anchor', 'end')
+    .attr('y', function (d, i) {
+      return ((ticks.length - 1) - i) * (height / (ticks.length - 1)) + y
+    })
+    .text(function (d, i) {
+      if (i % 2 === 0) {
+        if (d >= 1000) return Math.floor((d / 1000)) + ',' + (d % 1000).toString().padStart(3, '0')
+        return d
+      }
+    })
 }
