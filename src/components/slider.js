aa1fbe29c2d73ccbb84b77e79643001747ae4832
tricarbonate <reibell.theo@gmail.com>
2023-06-17 11:00:37 -0400

    updateSelectedDate callback to update plot

--- src/components/slider.js
+++ src/components/slider.js
@@ -12,26 +12,32 @@
  *                            This function takes one parameter containing the slider options.
  *                            Check the API documentation for more details.
  */
-export function append (parent, min, max, onChange) {
-  const slider = document.createElement('input')
-  slider.setAttribute('type', 'text')
-  parent.appendChild(slider)
+export function append(parent, min, max, onChange) {
+  const slider = document.createElement("input");
+  slider.setAttribute("type", "text");
+  parent.appendChild(slider);
 
   $(slider).ionRangeSlider({
-    skin: 'round',
-    type: 'double',
+    skin: "round",
+    type: "double",
     min: min.valueOf(),
     max: max.valueOf(),
     hide_min_max: true,
     hide_from_to: false,
     force_edges: true,
     prettify: function (ts) {
-      return new Date(ts).toLocaleDateString('en', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      })
+      return new Date(ts).toLocaleDateString("en", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
     },
-    onChange: onChange
-  })
+    onChange: (data) => {
+      // transform data into dates object
+      onChange({
+        from: new Date(data.from),
+        to: new Date(data.to),
+      });
+    },
+  });
 }
