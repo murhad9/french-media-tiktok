5272a55dafc57c4f121865b2195825dea17c7d5c
AugstinLompo <95591835+AugustinLompo@users.noreply.github.com>
2023-06-21 18:36:24 -0400

    modif

---
+++ src/video-length/scripts/viz-addons.js
@@ -0,0 +1,63 @@
+
+/**
+ * Displays the information panel when a data point is clicked.
+ *
+ * @param {object} d The data bound to the clicked marker
+ */
+export function displayPanel(d) {
+  const panel = d3.select("#video-panel").style("visibility", "visible");
+
+  const data = d.target.__data__;
+
+  panel.selectAll("*").remove();
+
+
+  // Title
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "18px")
+    .style("font-weight", "bold")
+    .style("padding-top", "25px")
+    .text(`${data.intervalle1}s to ${data.intervalle2}s`);
+  
+  // Number of videos
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "16px")
+    .style("padding-top", "25px")
+    .text(`Number of videos: ${Math.round(data.count)}`);
+
+  // Number of views
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "16px")
+    .style("padding-top", "25px")
+    .text(`Total Views: ${Math.round(data.vues)}`);
+
+  // Average views
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "16px")
+    .style("padding-top", "3px")
+    .text(`Total likes: ${Math.round(data.likes)}`);
+
+  // Average likes
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "16px")
+    .style("padding-top", "3px")
+    .text(`Total comments: ${Math.round(data.commentaires)}`);
+
+  // Average comments
+  panel
+    .append("div")
+    .style("font-family", "Roboto")
+    .style("font-size", "16px")
+    .style("padding-top", "3px")
+    .text(`Total shares: ${Math.round(data.partages)}`);
+}
